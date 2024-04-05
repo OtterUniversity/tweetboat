59814743c4b68582e4995642fd43638e24f535b7
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2024-04-05 02:43:21 -0600

    Grammar

--- src/cache.rs
+++ src/cache.rs
@@ -11,20 +11,20 @@ type MessageId = Id<MessageMarker>;
 /// This cache is backed by a ring buffer of a fixed number of ID pairs, and as
 /// it reaches capacity, the eldest element is evicted. Once an entry has been
 /// written, it cannot be removed until it gets popped out by a newer one.
-/// 
+///
 /// # Pending Entries
 /// As soon as a message that needs processing is received, it is entered into
 /// the cache in a [pending] state. This allows the bot to know that a message
 /// is relevant in the edit event, and to suppress embeds if the unfurler gets
 /// back to us before our reply has been sent out.
-/// 
+///
 /// Upon entering a pending state, an [InsertToken] is provided, which can be
 /// used once the reply has gone through to finish off the entry. Entries are
 /// allowed to stay in the pending state forever if the reply fails -- they will
 /// just eventually be evicted from cache once they end up at its tail. When a
 /// message is deleted, it may also be returned to a pending state via the
 /// [take_entry] method.
-/// 
+///
 /// [pending]: CacheEntry::Pending
 /// [take_entry]: ReplyCache::take_entry
 pub struct ReplyCache(VecDeque<(MessageId, CacheEntry)>);
@@ -41,7 +41,7 @@ impl ReplyCache {
     }
 
     /// Files a [CacheEntry::Pending] value for the given source message. If the
-    /// entry is free, a [InsertToken] is returned. If there is another value
+    /// entry is free, an [InsertToken] is returned. If there is another value
     /// in the source message's slot, `[None]` is returned.
     pub fn file_pending(&mut self, source: MessageId) -> Option<InsertToken> {
         if self.0.len() == self.0.capacity() {
@@ -89,7 +89,7 @@ impl ReplyCache {
     pub fn get_entry(&self, source: MessageId) -> Option<CacheEntry> {
         self.search(source).ok().map(|idx| self.0[idx].1)
     }
-    
+
     /// Gets an entry from the cache, invalidating it after it has been returned.
     pub fn take_entry(&mut self, source: MessageId) -> Option<CacheEntry> {
         if let Ok(idx) = self.search(source) {
@@ -126,7 +126,7 @@ pub enum CacheEntry {
     Pending,
     /// A filled entry pointing to the bot's reply message ID.
     Filled(MessageId),
-    
+
 }
 
 /// A token indicating that a message has been received and needs a reply but
@@ -163,33 +163,33 @@ mod tests {
         // Out of order insertion (2 then 1)
         let token = cache.file_pending(id!(2)).unwrap();
         assert_eq!(cache.get_entry(id!(2)), Some(CacheEntry::Pending));
-        
+
         cache.insert(token, id!(12));
         assert_eq!(cache.get_entry(id!(2)), Some(CacheEntry::Filled(id!(12))));
-        
+
         let token = cache.file_pending(id!(1)).unwrap();
         assert_eq!(cache.get_entry(id!(1)), Some(CacheEntry::Pending));
-        
+
         cache.insert(token, id!(11));
         assert_eq!(cache.get_entry(id!(1)), Some(CacheEntry::Filled(id!(11))));
-        
+
         // Quick succession: 2 messages come in before the 1st can be replied
         // to, and they are out of order
         let token_4 = cache.file_pending(id!(4)).unwrap(); // idx: 2
         let token_3 = cache.file_pending(id!(3)).unwrap(); // idx: 3
-        
+
         // Insert them back in order
         cache.insert(token_3, id!(13));
         cache.insert(token_4, id!(14));
-        
+
         assert_eq!(cache.get_entry(id!(3)), Some(CacheEntry::Filled(id!(13))));
         assert_eq!(cache.get_entry(id!(4)), Some(CacheEntry::Filled(id!(14))));
-        
+
         assert_eq!(cache.len(), 4);
-        
+
         let token = cache.file_pending(id!(5)).unwrap();
         cache.insert(token, id!(15));
-        
+
         // Hit capacity, evicted
         assert_eq!(cache.len(), 4);
         assert_eq!(cache.get_entry(id!(5)), Some(CacheEntry::Filled(id!(15))));
