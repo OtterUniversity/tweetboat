0ce390f4789e7b62bb4fe27f4d7505f31058c498
Ben Richeson <36977340+Benricheson101@users.noreply.github.com>
2024-12-24 18:03:49 -0500

    add config option for removing query parameters (#1)

--- tests/passes.rs
+++ tests/passes.rs
@@ -8,7 +8,7 @@ fn standard_passes() {
         pass.extract(
             "
                 These are just some random test urls.
-                - https://x.com/rustbeltenjoyer/status/1776056709737320578
+                - https://x.com/rustbeltenjoyer/status/1776056709737320578?s=46&t=owouwu
                 - ||https://www.instagram.com/p/C5W2QwZrt-Z/ ||
                 - https://www.tiktok.com/t/ZPRTX3AwH/
             ",
@@ -21,14 +21,18 @@ fn standard_passes() {
         extracted.next(),
         Some((
             "/rustbeltenjoyer/status/1776056709737320578",
+            "s=46&t=owouwu",
             SpoilerTags::None
         ))
     );
     assert_eq!(
         extracted.next(),
-        Some(("/p/C5W2QwZrt-Z/", SpoilerTags::Spoiler))
+        Some(("/p/C5W2QwZrt-Z/", "", SpoilerTags::Spoiler))
     );
-    assert_eq!(extracted.next(), Some(("/t/ZPRTX3AwH/", SpoilerTags::None)));
+    assert_eq!(
+        extracted.next(),
+        Some(("/t/ZPRTX3AwH/", "", SpoilerTags::None))
+    );
 
     assert!(extracted.next().is_none());
 }
