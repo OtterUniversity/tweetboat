f1e6145090531c4e71dad687d51965cfdbdae592
Ben Richeson <benricheson101@gmail.com>
2024-12-24 17:52:54 -0500

    add config option for removing query parameters

--- src/pass.rs
+++ src/pass.rs
@@ -1,4 +1,4 @@
-use std::fmt::Write;
+use std::{collections::HashMap, fmt::Write};
 
 use regex::Regex;
 use serde::{Deserialize, Deserializer};
@@ -9,6 +9,7 @@ pub struct Pass {
     #[serde(deserialize_with = "pass_regex")]
     pub regex: Regex,
     pub stem: String,
+    pub keep_query: Option<Vec<String>>,
 }
 
 /// An enum representing the spoiler tags on a link.
@@ -25,7 +26,10 @@ pub enum SpoilerTags {
 }
 
 impl Pass {
-    pub fn extract<'a>(&'a self, content: &'a str) -> impl Iterator<Item = (&'a str, SpoilerTags)> {
+    pub fn extract<'a>(
+        &'a self,
+        content: &'a str,
+    ) -> impl Iterator<Item = (&'a str, &'a str, SpoilerTags)> {
         self.regex.captures_iter(content).map(|capture| {
             let (_, [sp_open, path, sp_close]) = capture.extract();
             let spoiler_marker = match (!sp_open.is_empty(), !sp_close.is_empty()) {
@@ -34,28 +38,36 @@ impl Pass {
                 _ => SpoilerTags::Mismatched,
             };
 
-            (path, spoiler_marker)
+            let (path, query) = path.split_once('?').unwrap_or((path, ""));
+
+            (path, query, spoiler_marker)
         })
     }
 
     pub fn apply<'a>(&'a self, content: &'a str) -> Option<String> {
         let Self { label, stem, .. } = self;
 
-        let out = self
-            .extract(content)
-            .fold(String::new(), |mut out, (path, spoiler_tags)| {
-                let spoil = spoiler_tags != SpoilerTags::None;
+        let out =
+            self.extract(content)
+                .fold(String::new(), |mut out, (path, query, spoiler_tags)| {
+                    let spoil = spoiler_tags != SpoilerTags::None;
 
-                if spoil {
-                    let _ = write!(&mut out, "||");
-                }
-                let _ = write!(&mut out, "[`{label}`]({stem}{path}) ");
-                if spoil {
-                    let _ = write!(&mut out, "|| ");
-                }
+                    let query_string = match &self.keep_query {
+                        None => format!("?{query}"),
+                        Some(keep) if !keep.is_empty() => filter_query(query, keep),
+                        _ => String::new(),
+                    };
 
-                out
-            });
+                    if spoil {
+                        let _ = write!(&mut out, "||");
+                    }
+                    let _ = write!(&mut out, "[`{label}`]({stem}{path}{query_string}) ");
+                    if spoil {
+                        let _ = write!(&mut out, "|| ");
+                    }
+
+                    out
+                });
 
         (!out.is_empty()).then_some(out)
     }
@@ -80,4 +92,17 @@ fn pass_regex<'de, D: Deserializer<'de>>(de: D) -> Result<Regex, D::Error> {
     let core = String::deserialize(de)?;
     Regex::new(&["(?:^|\\s)", "(\\|\\||)", &core, "(/\\S+)", "(\\s?\\|\\||)"].concat())
         .map_err(D::Error::custom)
+}
+
+/// Removes all query parameters from a query string except those in the provided list
+fn filter_query(qs: &str, keep: &Vec<String>) -> String {
+    let query_map: HashMap<_, _> = qs.split('&').filter_map(|p| p.split_once('=')).collect();
+
+    let params = query_map
+        .iter()
+        .filter_map(|(k, v)| keep.contains(&k.to_string()).then(|| format!("{k}={v}")))
+        .collect::<Vec<_>>()
+        .join("&");
+
+    format!("?{params}")
 }
